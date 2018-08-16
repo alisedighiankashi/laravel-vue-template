90ebb951993b3b48dfcbf6966dec1990e591896b
ali sedighian <ali.sedighian.kashi@gmail.com>
2018-08-16 20:36:26 +0430

    laravel init

---
+++ resources/assets/js/app.js
@@ -0,0 +1,48 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+
+import * as filters from './filters' // global filters
+
+Vue.use(Element, {
+    size: 'medium', // set element-ui default size
+    i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key])
+})
+
+Vue.config.productionTip = false
+
+const app =new Vue({
+    el: '#app',
+    router,
+    store,
+    i18n,
+    render: h => h(App)
+})
